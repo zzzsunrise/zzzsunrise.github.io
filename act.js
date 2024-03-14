c18b2fd9b0f966c373a78f5d6d91d8fdfef9b097
zzzsunrise <113669170+zzzsunrise@users.noreply.github.com>
2024-03-14 12:48:57 +0900

    슬라이더 동작 구현

--- act.js
+++ act.js
@@ -1,5 +1,5 @@
 // 슬라이더와 숫자 텍스트 엘리먼트를 가져옵니다.
-const slider = document.getElementById('slider');
+const slider = document.getElementById('slider1');
 const sliderValue = document.getElementById('sliderValue');
 
 // 슬라이더의 값이 변경될 때마다 이벤트를 추가합니다.
